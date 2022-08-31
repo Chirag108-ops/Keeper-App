2b9603ee3619de2550f617c71516f87502ee5f7d
Chirag Goyal <chiraggoyal@Chirags-MacBook-Air.local>
2022-08-31 18:13:58 +0530

    Keeper App project using React Js

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HieghlightIcon from '@material-ui/icons/Highlight'
+function Header() {
+  return (
+    <header>
+      <h1> <HieghlightIcon/>Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
